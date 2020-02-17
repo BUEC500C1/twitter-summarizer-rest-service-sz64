91edad85be8acfdbe9567f6be928b0e3962fca5b
Steven <sz64@bu.edu>
2020-02-17 18:44:04 -0500

    module now pulls tweets and image links

--- tweet_puller.py
+++ tweet_puller.py
@@ -1,7 +1,7 @@
 import tweepy
 import datetime
 
-def tweet_pull(scr_name, n_tweets):
+def tweet_pull(scr_name):
 
 	keys = open("TKey.txt").read().split();
 	consumer_key = keys[0];
@@ -11,16 +11,16 @@ def tweet_pull(scr_name, n_tweets):
 
 	api = tweepy.API(auth)
 	
+	n_tweets = 20;
 	tweets = [];
 	types = [];
 	for tweet in tweepy.Cursor(api.user_timeline, screen_name=scr_name, result_type = 'recent', count = n_tweets, tweet_mode='extended').items(n_tweets):
-		im = tweet.entities.get('media', []);
-		tweets.append(tweet.full_text);
-		types.append('0');
-		if (len(im) > 0):
-			tweets.append(im[0]['media_url']);
-			types.append('1');
-		# This is a place holder for determining if the tweet was from today. 
-		print(tweet.created_at.date() == datetime.datetime.now().date())
+		if tweet.created_at.date() == datetime.datetime.now().date():
+			im = tweet.entities.get('media', []);
+			tweets.append(tweet.full_text);
+			types.append('0');
+			if (len(im) > 0):
+				tweets.append(im[0]['media_url']);
+				types.append('1');
 		
 	return tweets, type;
